ba69cdc74cc0c8b3e171834b8677d7ad21838cd3
Gijs Reijn <gijsreijn@hotmail.com>
2024-09-12 09:34:29 +0200

    Looking for depth

--- tests/Unit/Public/ConvertTo-DscJson.tests.ps1
+++ tests/Unit/Public/ConvertTo-DscJson.tests.ps1
@@ -28,14 +28,14 @@ configuration MyConfiguration {
 
         It 'Should return valid JSON' {
             $json = ConvertTo-DscJson -Path $filePath
-            $out = $json | ConvertFrom-Json
+            $out = $json | ConvertFrom-Json -Depth 10
             $out.resources.name | Should -Be 'MyConfiguration'
             $out.resources.type | Should -BeLike '*PowerShell*'
-            # $out.resources.properties.resources.properties.Name | Should -Not -BeNullOrEmpty
-            # $out.resources.properties.resources.properties.Value | Should -Not -BeNullOrEmpty
-            # $out.resources.properties.resources.properties.Ensure | Should -Not -BeNullOrEmpty
-            # $out.resources.properties.resources.properties.Target | Should -Not -BeNullOrEmpty
-            # $out.resources.properties.resources.properties.Path | Should -Not -BeNullOrEmpty
+            $out.resources.properties.resources.properties.Name | Should -Not -BeNullOrEmpty
+            $out.resources.properties.resources.properties.Value | Should -Not -BeNullOrEmpty
+            $out.resources.properties.resources.properties.Ensure | Should -Not -BeNullOrEmpty
+            $out.resources.properties.resources.properties.Target | Should -Not -BeNullOrEmpty
+            $out.resources.properties.resources.properties.Path | Should -Not -BeNullOrEmpty
         }
     }
 }
