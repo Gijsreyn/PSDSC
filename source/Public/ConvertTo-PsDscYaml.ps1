5fd1e616ed7590a03f8941704c15ed9cac4d0f8b
Gijs Reijn <gijsreijn@hotmail.com>
2025-02-28 09:03:12 +0100

    Add new psdscyaml

---
+++ source/Public/ConvertTo-PsDscYaml.ps1
@@ -0,0 +1,101 @@
+function ConvertTo-PsDscYaml
+{
+    <#
+    .SYNOPSIS
+        Convert DSC Configuration (v1/v2) Document to YAML.
+
+    .DESCRIPTION
+        The function ConvertTo-PsDscYaml converts a DSC Configuration Document (v1/v2) to YAML.
+
+    .PARAMETER Path
+        The file path to a valid DSC Configuration Document.
+
+    .PARAMETER Content
+        The content to a valid DSC Configuration Document.
+
+    .EXAMPLE
+        PS C:\> $path = 'myConfig.ps1'
+        PS C:\> ConvertTo-PsDscYaml -Path $path
+
+    .INPUTS
+        Input a valid DSC Configuration Document
+
+        configuration MyConfiguration {
+            Import-DscResource -ModuleName PSDesiredStateConfiguration
+            Node localhost
+            {
+                Environment CreatePathEnvironmentVariable
+                {
+                    Name = 'TestPathEnvironmentVariable'
+                    Value = 'TestValue'
+                    Ensure = 'Present'
+                    Path = $true
+                    Target = @('Process')
+                }
+            }
+        }
+
+    .OUTPUTS
+        Returns a YAML string
+        $schema: https://raw.githubusercontent.com/PowerShell/DSC/main/schemas/2024/04/config/document.json
+        resources:
+            name: MyConfiguration
+            type: Microsoft.DSC/PowerShell
+            properties:
+                resources:
+                - name: CreatePathEnvironmentVariable
+                type: PSDscResources/Environment
+                properties:
+                    Value: TestValue
+                    Path: true
+                    Name: TestPathEnvironmentVariable
+                    Ensure: Present
+                    Target:
+                    - Process
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Path')]
+    [OutputType([System.String])]
+    param
+    (
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Path')]
+        [ValidateScript({
+                if (-Not ($_ | Test-Path) )
+                {
+                    throw "File or folder does not exist"
+                }
+                if (-Not ($_ | Test-Path -PathType Leaf) )
+                {
+                    throw "The Path argument must be a file. Folder paths are not allowed."
+                }
+                return $true
+            })]
+        [System.String]
+        $Path,
+
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Content')]
+        [System.String]
+        $Content
+    )
+
+    begin
+    {
+        Write-Verbose -Message ("Starting: {0}" -f $MyInvocation.MyCommand.Name)
+    }
+
+    process
+    {
+        $configurationDocument = Build-DscConfigDocument @PSBoundParameters
+    }
+
+    end
+    {
+        Write-Verbose ("Ended: {0}" -f $MyInvocation.MyCommand.Name)
+        if (-not (Get-Module -ListAvailable yayaml -ErrorAction SilentlyContinue) -or (Get-Module -ListAvailable powershell-yaml))
+        {
+            $inputObject = ConvertTo-Yaml -InputObject $configurationDocument -Depth 10
+        }
+        return $inputObject
+    }
+}
